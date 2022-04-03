2cf9f7707a5a716a045a9efa90ed3ff85806100a
Дарья Тимохина <c0mdas@yandex.ru>
2022-04-03 15:10:53 +0300

    yatube project 15.10

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -9,5 +9,9 @@ def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
 
 
+def permission_denied(request, exception):
+    return render(request, 'core/403.html', status=403)
+
+
 def server_error(request):
-    return render(request, 'core/500.html')
+    return render(request, 'core/500.html', status=500)
